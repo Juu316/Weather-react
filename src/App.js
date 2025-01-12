1098b4a95ce1c69cdf7863781142f95398366fb6
Usukh-Erdene Choijamts <uujuu316@gmail.com>
2025-01-12 13:06:27 +0800

    initial commit from home windows pc

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font text-2xl text-center">Hello World</h1>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
