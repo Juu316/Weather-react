2d708c020f6310c97fc992263bfd15e3e172ee8f
Usukh-Erdene Choijamts <uujuu316@gmail.com>
2025-01-14 12:31:15 +0800

    class commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,83 @@
 import { useEffect, useState } from "react";
 import search from "./svg/search.svg";
+
+const countriesUrl = "https://countriesnow.space/api/v0.1/countries";
+const weatherUrl = "https://api.weatherapi.com/v1/forecast.json?";
+
 function App() {
+  const [cities, setCities] = useState([]);
   const [citiesSearch, setCitiesSearch] = useState("");
-  const [cities, setCities] = useState();
+  const [filteredData, setFilteredData] = useState([]);
+
   const handleChange = (event) => {
     setCitiesSearch(event.target.value);
   };
-  const [filteredData, setFilteredData] = useState([]);
-  const fetchData = () => {
-    fetch("https://countriesnow.space/api/v0.1/countries")
+
+  const fetchCities = () => {
+    fetch(countriesUrl)
       .then((response) => response.json())
       .then((result) => {
-        console.log(result);
+        //  CitiesAndCountries()
+        setFilteredData(result.data);
+        setCities(result.data);
       })
       .catch((error) => {
         console.log("error", error);
       });
   };
+function toLowerCase(value){
+ return value.toLowerCase();
+}
   const filterData = () => {
-    setFilteredData();
+    setFilteredData(cities.filter((city) => {
+      toLowerCase(city.cities)
+    }));
+  };
+
+  const seperateAndGetCities = () => {
+    cities.map((obj) => {
+      return obj.cities;
+    });
   };
   useEffect(() => {
-    fetchData();
+    console.log("get cities function worked");
+    seperateAndGetCities();
+  }, []);
+  useEffect(() => {
     console.log("Fetch data worked");
+    fetchCities();
+  }, []);
+
+  useEffect(() => {
+    filterData();
   }, [citiesSearch]);
+
   return (
     <div className="box-border">
+      <div className="flex justify-center">
+        <div
+          id="containerOfSearchBar"
+          className="h-16 rounded-3xl w-auto flex z-10 absolute top-10">
+          <img
+            className="block z-10 relative left-16"
+            alt="search icon"
+            src={search}
+          />
+          <input
+            className="rounded-3xl text-4xl"
+            onChange={handleChange}
+            placeholder="          Search"
+          />
+        </div>
+      </div>
+
       <div className="flex ">
         <div className="h-screen w-1/2 bg-gray-100  border-black">
-          <div id='containerOfSearchBar' className="h-16 rounded-3xl ml-32 mt-20 w-96 flex">
-           <img className ="block" alt="search icon" src={search}/>
-           
-            <input
-            className=""
-              onChange={handleChange}
-              placeholder=" Search"
-            />
-          </div>
+        <ul>
+        {filteredData.map((city) => (
+          <li key={city.id}>{city.name}</li>
+        ))}
+      </ul>
         </div>
         <div className="h-screen w-1/2 bg-custom-color"></div>
       </div>
